28f3eb2ea3b706418c779d3c042430e3c101d448
Pikoon <gaga.guetta@laposte.net>
2024-08-28 07:53:44 -0400

    TP3

--- compteurcouches.py
+++ compteurcouches.py
@@ -16,11 +16,15 @@ try:
     indexCarte=int(input('Quel est l\'index de la carte à capturer  ?'))
     packets=sniff(count=nbPackets,iface=dev_from_index(indexCarte))
     compteur=0
+    compteurTotal=0
     for packet in packets :
+        compteurTotal+=1
         if(packet.haslayer(coucheASniffer)):
             compteur+=1
 
     print('La capture contient '+str(compteur)+' paquets du type '+coucheASniffer)
+    pourcentage = (compteur/compteurTotal)*100
+    print('Cela représente '+str(pourcentage)+"% des requêtes totales.")
 except ValueError as e:
     print("La couche que vous souhaitez capturer n'existe pas. Le script s'interrompt.")
     SystemExit(1)
